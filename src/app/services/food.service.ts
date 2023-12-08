4f27e10376bdd289445de52c281c34ccc33affc1
FatiBouya <fatibouya12@gmail.com>
2023-12-08 19:35:52 -0400

    Add api getAll and getOne on food service

--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -12,4 +12,15 @@ export class FoodService {
   getAll():Food[]{
     return sample_foods;
   }
+  
+
+  getSearchFoodsByname(searchName:string){
+
+    return this.getAll().filter(food=>food.name.toLowerCase().includes(searchName.toLowerCase()))
+
+  }
+  getFoodsById(foodId:number):Food{
+    return this.getAll().find(food=>food.id==foodId)?? new Food();
+  }
+ 
 }
